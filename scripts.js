f2eea85ad54f7095706399e90ec3b525018796d5
Chris Underwood <cunderwoodmn@gmail.com>
2018-01-05 21:43:54 -0800

    add rgba split which displays

--- scripts.js
+++ scripts.js
@@ -26,13 +26,13 @@ function paintToCanvas() {
  // take the pixels out
  let pixels = ctx.getImageData(0, 0, width, height);
  // mess with them
- pixels = redEffect(pixels);
+ //pixels = redEffect(pixels);
 //change pixels
  //pixels = rgbSplit(pixels);
  //put pixels back
- //ctx.globalAlpha = 0.8;
+ //ctx.globalAlpha = 0.1;
 
- // pixels = greenScreen(pixels);
+ pixels = greenScreen(pixels);
  // put them back
  ctx.putImageData(pixels, 0, 0);
     }, 16);
@@ -62,6 +62,41 @@ function redEffect(pixels) {
   return pixels;
 }
 
+function rgbSplit(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4) {
+    pixels.data[i - 150] = pixels.data[i + 0]; // RED
+    pixels.data[i + 500] = pixels.data[i + 1]; // GREEN
+    pixels.data[i - 550] = pixels.data[i + 2]; // Blue
+  }
+  return pixels;
+}
+
+function greenScreen(pixels) {
+  const levels = {};
+
+  document.querySelectorAll('.rgb input').forEach((input) => {
+    levels[input.name] = input.value;
+  });
+
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0];
+    green = pixels.data[i + 1];
+    blue = pixels.data[i + 2];
+    alpha = pixels.data[i + 3];
+
+    if (red >= levels.rmin
+      && green >= levels.gmin
+      && blue >= levels.bmin
+      && red <= levels.rmax
+      && green <= levels.gmax
+      && blue <= levels.bmax) {
+      // take it out!
+      pixels.data[i + 3] = 0;
+    }
+  }
+
+  return pixels;
+}
 
 getVideo();
 
